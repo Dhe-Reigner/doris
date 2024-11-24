66b82955c14ccc2fb734261ff615d7423b2e3c54
Dhe-Reigner <martinkagema01@gmail.com>
2024-11-24 21:55:59 +0300

    Initial commit

--- Events/urls.py
+++ Events/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .import views
 
-urlpatterns =[
-    path('', views.home, name='home')
-
-]
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('participants/', views.participants, name='participants')
+]  
--- Events/views.py
+++ Events/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import User
+from .models import Event
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    all_place = Event.objects.all()
+    return render(request, 'home.html',{
+        'tour':all_place
+    })
+    
+def participants(request):
+    all_attendees = User.objects.all()
+    return render(request, 'participants.html',{
+        'out':all_attendees
+    })
